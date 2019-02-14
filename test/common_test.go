934080ff17b106c1525cfba8ce442c6708a40c94
davis_chen <davis_chen@ubiqconn.com>
2019-02-14 15:39:30 +0800

    1. Enable flag 2. Refactory flow

--- test/common_test.go
+++ test/common_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"flag"
 	"io/ioutil"
+	"log"
 	"main/common"
 	"net/http"
 	"strconv"
@@ -11,14 +12,26 @@ import (
 	"time"
 )
 
-func TestInsertData(t *testing.T) {
-	common.InitRedis("127.0.0.1:6379")
+var (
+	mRedisIp    = flag.String("redisip", "127.0.0.1", "redis ip")
+	mRedisPort  = flag.String("redisport", "6379", "redis port")
+	mServerPort = flag.String("port", "10034", "server port")
+)
 
+func TestInsertData(t *testing.T) {
+	flag.Parse()
+
+	addr := *mRedisIp + ":" + *mRedisPort
+	t.Logf("Connect server %s", addr)
+	common.InitRedis(addr)
+
+	defer common.DeleteIP("192.168.0.0")
 	// try insert 60 requests
 	for i := 1; i <= 60; i++ {
 		n, err := common.InsertRequest("192.168.0.0")
 		if err != nil {
 			t.Errorf("get error %q when insert %d", err.Error(), i)
+			return
 		} else if n != i {
 			t.Errorf("request number is not match %d but get %d", i, n)
 		}
@@ -29,34 +42,33 @@ func TestInsertData(t *testing.T) {
 	if n != 61 || err == nil {
 		t.Errorf("the error should be return when insert 61'th requests but get %d err %q", n, err.Error())
 	}
-
-	common.DeleteIP("192.168.0.0")
 }
 
-func insertHTTPRequestInternel(t *testing.T, addr string) {
+func insertHTTPRequestInternel(t *testing.T, addr string) error {
 
 	// try insert 60 requests
 	for i := 1; i <= 60; i++ {
 		resp, err := http.Get(addr)
 		if err != nil {
 			t.Errorf("Connect server failed %q", err.Error())
-			return
+			return err
 		}
 		body, err := ioutil.ReadAll(resp.Body)
 		n, err := strconv.Atoi(string(body[:]))
 		if err != nil {
 			t.Errorf("get error %q when insert %d", err.Error(), i)
+			return err
 		} else if n != i {
 			t.Errorf("request number is not match %d but get %d", i, n)
 		}
 		resp.Body.Close()
 	}
 
-	// error should be returned
+	// "error" should be returned
 	resp, err := http.Get(addr)
 	if err != nil {
 		t.Errorf("Connect server failed %q", err.Error())
-		return
+		return err
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	s := string(body[:])
@@ -65,14 +77,17 @@ func insertHTTPRequestInternel(t *testing.T, addr string) {
 	}
 
 	resp.Body.Close()
+	return nil
 }
 
 func TestInsertHTTPRequest(t *testing.T) {
-	port := flag.String("port", "10034", "server port")
 	flag.Parse()
-	addr := "http://:" + *port
-	insertHTTPRequestInternel(t, addr)
-	// sleep 61 second and test again ti check timeout
+	addr := "http://:" + *mServerPort
+	err := insertHTTPRequestInternel(t, addr)
+	if err != nil {
+		return
+	}
+	log.Println("Sleep 61 second for expired and test again")
 	time.Sleep(time.Minute + time.Second)
-	insertHTTPRequestInternel(t, addr)
+	err = insertHTTPRequestInternel(t, addr)
 }
